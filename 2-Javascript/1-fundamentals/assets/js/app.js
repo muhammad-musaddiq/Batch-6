decf889d9c65ab8901fbf966c2f82570ec010527
Muhammad Musaddiq <muhammad.musaddiq@sipligroup.com>
2023-10-03 23:54:34 +0500

    [tuesday] learn data types

--- 2-Javascript/1-fundamentals/assets/js/app.js
+++ 2-Javascript/1-fundamentals/assets/js/app.js
@@ -1,3 +1,20 @@
-console.log(`1 js file working fine`);
-console.log("2 js file working fine");
-console.log("3 js file working fine");
+// console.log(`1 js file working fine`);
+// console.log("2 js file working fine");
+// console.log("3 js file working fine");
+
+// DATA TYPES
+
+// There are two types of data types
+// 1 - Primitive type
+// 2 - Reference type / object type
+
+// Primitive type variables
+
+var store = "something"; // string  = text
+var store = 1234; // number
+var store = true; // boolean (true/false)
+var store = false; // boolean (true/false)
+var store = null; // (khali hai)
+var store = undefined; // undefined
+
+console.log(storetest, "store");
