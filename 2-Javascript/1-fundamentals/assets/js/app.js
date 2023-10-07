0674f6277a0cc5757687d6d282f6ad065854395e
Muhammad Musaddiq <muhammad.musaddiq@sipligroup.com>
2023-10-07 17:59:42 +0500

    [saturday] learn data types - refreance and premitive types

--- 2-Javascript/1-fundamentals/assets/js/app.js
+++ 2-Javascript/1-fundamentals/assets/js/app.js
@@ -17,4 +17,35 @@ var store = false; // boolean (true/false)
 var store = null; // (khali hai)
 var store = undefined; // undefined
 
-console.log(storetest, "store");
+// console.log(storetest, "store");
+
+// Reference type
+
+var store1 = ["one", 123, true, false, null, undefined];
+var store = {
+  //key:value
+  one: "one",
+  two: "two",
+};
+
+// console.log(store1, "store1");
+// console.log(store, "store");
+
+var muzammilMarks = 90;
+var musaddiqMarks = muzammilMarks;
+
+musaddiqMarks = 95;
+
+// console.log(muzammilMarks, "muzammilMarks");
+// console.log(musaddiqMarks, "musaddiqMarks");
+
+// Reference type example
+
+var arrayOne = ["one", "two"];
+var arraytwo = arrayOne; // it will create a refrance of array One
+var arrayThree = arraytwo; // it will create a refrance of array One
+
+arraytwo.push("three");
+
+console.log(arrayOne, "arrayOne");
+console.log(arraytwo, "arraytwo");
