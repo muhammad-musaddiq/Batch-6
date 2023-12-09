702f121a2fe850956cada543d67f426c35bfb63f
Muhammad Musaddiq <muhammad.musaddiq@sipligroup.com>
2023-12-10 00:09:39 +0500

    [saturday] calculator project complete

--- 2-Javascript/3-dom-projects/1-calculator/app.js
+++ 2-Javascript/3-dom-projects/1-calculator/app.js
@@ -1,5 +1,7 @@
 const getALlButtonsElements = document.querySelectorAll(".btn");
 const screenInput = document.querySelector(".screen");
+const selectEqualBtn = document.querySelector(".btn-equal");
+const clearButton = document.querySelector(".btn-clear");
 
 getALlButtonsElements.forEach(function (singleButton) {
   singleButton.addEventListener("click", function (event) {
@@ -13,3 +15,29 @@ getALlButtonsElements.forEach(function (singleButton) {
     console.log(currentElement, "currentElement");
   });
 });
+
+selectEqualBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  screenInput.value = eval(screenInput.value);
+});
+
+clearButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  screenInput.value = "";
+});
+
+screenInput.addEventListener("input", function (event) {
+  event.preventDefault();
+  const currentElement = event.target;
+  // it's mean you are entering the a to z so it will return true
+  if (onlyLetters(currentElement.value)) {
+    currentElement.value = "";
+  }
+});
+
+// regular expression
+// regex
+
+function onlyLetters(str) {
+  return /[a-zA-z]+$/.test(str);
+}
