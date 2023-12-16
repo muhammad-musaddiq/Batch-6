e0af64cfe094fd03d61a35b6ef42fcbee702d79d
Muhammad Musaddiq <muhammad.musaddiq@sipligroup.com>
2023-12-16 18:05:25 +0500

    [Saturday] already item add logic

--- 2-Javascript/4-cart-project/store.js
+++ 2-Javascript/4-cart-project/store.js
@@ -13,7 +13,43 @@ function addtoCardBtnHandler(event) {
   const itemImage = mainShopItemDiv.querySelector(".shop-item-image").src;
   const itemPrice = mainShopItemDiv.querySelector(".shop-item-price").innerText;
 
-  cartItemElement.innerHTML = "teafwefawed";
+  const cartItemNames = document.querySelectorAll(".cart-item-title");
+
+  let isCartItemIsAlreadyExist = false;
+
+  if (cartItemNames.length > 0) {
+    cartItemNames.forEach(function (singleCartItemName) {
+      if (singleCartItemName.innerText === itemName) {
+        isCartItemIsAlreadyExist = true;
+      }
+    });
+  }
+
+  if (isCartItemIsAlreadyExist) {
+    alert("This item is already exist in our cart");
+    return;
+  }
+
+  cartItemElement.innerHTML += `<div class="cart-row">
+          <div class="cart-item cart-column">
+            <img
+              class="cart-item-image"
+              src=${itemImage}
+              width="100"
+              height="100"
+            />
+            <span class="cart-item-title">${itemName}</span>
+          </div>
+          <span class="cart-column"
+            >$ <span class="cart-price-item-item">${itemPrice}</span></span
+          >
+          <div class="cart-quantity cart-column">
+            <input class="cart-quantity-input" type="number" value="1" />
+            <button class="btn btn-danger btn-remove" type="button">
+              REMOVE
+            </button>
+          </div>
+        </div>`;
 
   console.log(itemName, "itemName");
   console.log(itemImage, "itemImage");
